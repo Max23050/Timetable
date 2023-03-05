409af1a1caa829d9ee77ce250e4bac5232753744
Max <maxtaran708@gmail.com>
2023-03-05 23:51:50 +0200

    js update

--- js/script.js
+++ js/script.js
@@ -1,61 +1,45 @@
-const open_tuesday = document.querySelectorAll('.open-tuesday');
-const open_wednesday = document.querySelectorAll('.open-wednesday');
-const close = document.querySelectorAll('.close');
-const modal_container = document.getElementById('modal_container');
-const modal_tuesday = document.getElementById('modal_tuesday');
-const modal_wednesday = document.getElementById('modal_wednesday');
-const wrapper_even = document.querySelector('.timetable__wrapper-even');
-const wrapper_odd = document.querySelector('.timetable__wrapper-odd');
-const saturday_monday = document.querySelector('.timetable__block-mon');
-const saturday_tuesday = document.querySelector('.timetable__block-tue');
-const saturday_wednesday = document.querySelector('.timetable__block-wed');
-const saturday_thursday = document.querySelector('.timetable__block-thu');
-const saturday_friday = document.querySelector('.timetable__block-fri');
-
-
+const openTuesday = document.querySelectorAll('.open-tuesday');
+const closeModal = document.querySelectorAll('.close');
+const modalContainer = document.getElementById('modal_container');
+const modalTuesday = document.getElementById('modal_tuesday');
 
 
 // Modal
 
+function close() {
+    modalContainer.classList.remove('show');
+    modalTuesday.classList.remove('show');
+}
 
-open_tuesday.forEach(function (item) {
+
+openTuesday.forEach((item) => {
     item.addEventListener('click', function() {
-        modal_container.classList.add('show');
-        modal_tuesday.classList.add('show');
+        modalContainer.classList.add('show');
+        modalTuesday.classList.add('show');
     });
 });
 
-close.forEach(function (item) {
-    item.addEventListener('click', function() {
-        modal_container.classList.remove('show');
-        modal_tuesday.classList.remove('show');
-        modal_wednesday.classList.remove('show');
-    });
+closeModal.forEach((item) => {
+    item.addEventListener('click', close);
 });
 
 
-open_wednesday.forEach(function (item) {
-    item.addEventListener('click', function() {
-        modal_container.classList.add('show');
-        modal_wednesday.classList.add('show');
-    });
-});
-
-window.onclick = function (e) {
-    if (e.target == modal_container) {
-        modal_container.classList.remove('show');
-        modal_tuesday.classList.remove('show');
-        modal_wednesday.classList.remove('show');
-    };
+window.onclick = (e) => {
+    if (e.target == modalContainer) {
+        close();
+    }
 };
 
 
 // Even / odd week
 
+const wrapperEven = document.querySelector('.timetable__wrapper-even');
+const wrapperOdd = document.querySelector('.timetable__wrapper-odd');
+
 
 let DateTime = luxon.DateTime;
 const week = DateTime.now().weekNumber;
-console.log(week)
+console.log(week);
 
 
 function setSelectValue (id, val) {
@@ -64,54 +48,27 @@ function setSelectValue (id, val) {
 
 if ( week % 2 == 0 ) {
     console.log('Четная');
-    wrapper_even.classList.add('current');
-    wrapper_odd.classList.remove('current');
+    wrapperEven.classList.add('current');
+    wrapperOdd.classList.remove('current');
     setSelectValue('week', 'even');
 } else {
     console.log('Нечетная');
-    wrapper_odd.classList.add('current');
-    wrapper_even.classList.remove('current');
+    wrapperOdd.classList.add('current');
+    wrapperEven.classList.remove('current');
     setSelectValue('week', 'odd');
-};
-
-
-// Saturday script
-
-
-switch(week) {
-    case 45:
-      saturday_tuesday.classList.add('current_sat');
-      saturday_monday.classList.add('current_sat');
-      break;
-    case 46: 
-      saturday_wednesday.classList.add('current_sat');
-      saturday_tuesday.classList.add('current_sat');
-      break;
-    case 47: 
-      saturday_wednesday.classList.add('current_sat');
-      saturday_thursday.classList.add('current_sat');
-      break;
-    case 48: 
-      saturday_thursday.classList.add('current_sat');
-      saturday_friday.classList.add('current_sat');
-      break;
-    case 49: 
-    saturday_friday.classList.add('current_sat');
-    break;
-};
-
+}
 
 // Select odd / even 
 const select = document.querySelector('select');
 
 
-select.addEventListener('change', function() {
+select.addEventListener('change', () => {
     if (select.value == 'even' ) {
-        wrapper_even.classList.add('current');
-        wrapper_odd.classList.remove('current');
+        wrapperEven.classList.add('current');
+        wrapperOdd.classList.remove('current');
     } else if (select.value == 'odd') {
-        wrapper_odd.classList.add('current');
-        wrapper_even.classList.remove('current');
-    };
+        wrapperOdd.classList.add('current');
+        wrapperEven.classList.remove('current');
+    }
 });
 
